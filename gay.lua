45e44d7f4d38d56a595109a25713cc3a591ba981
Mashiro Shiina <80926013+JoonyWoony@users.noreply.github.com>
2022-08-14 16:04:22 +1000

    Update gay.lua

--- gay.lua
+++ gay.lua
@@ -1,31 +1,59 @@
-local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/dirt",true))()
-local Table = {}
-local window = Lib:CreateWindow("Joony's Nigger Hub | FUCK YOU DANIEL JADRIEN AND JASON UR LITTLE FAGS")
-window:Section("Tools")
-window:Toggle("Toggle",{location = Table, flag = "Toggle"},function()
-    getgenv().Loop = true
-end)
-window:Button("SLAP ON 6 & OFFICIAL",function()
-end)
-window:Slider("Slider",{location = Table, min = 1, max = 64, default = 32, precise = true --[[ 0.00 instead of 0 ]], flag = "Slider"},function()
-   print(Table["Slider"])
-end)
-window:Dropdown("Dropdown",{location = Table,flag = "Dropdown",search = true --[[AddsSearchBar]], list = {"1","2","3","4","5","6","7","8","9","0"} --[[Wont work when PlayerList = true]], PlayerList = true --[[ Turns the list into the players in the server ]]},function()
-   print(Table["Dropdown"])
-end)
-window:Bind("KeyBind",{location = Table, flag = "KeyBind", default = Enum.KeyCode.B},function() -- Automatically stops when the gui is removed
-    getgenv().Loop = false
-end)
-window:Search(Color3.fromRGB(255,0,255) --[[nil = Yellow]]) -- Ez searcher for if you have a lot of things
-window:String({string = "String"})
+local DiscordLib =
+    loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
 
-while wait() do -- while you wait it will do
-    if getgenv().Loop == true then -- if game environment is set to true then
-    game:GetService("ReplicatedStorage").Remotes.CallOfficial:FireServer()
-    
- -- continuously fires the remotes 
-        print("Slap on six auto") -- continuously prints, this can help for letting you know when it is off and on
-    else
-        print("off") -- continuously prints, this can help for letting you know when it is off and on
+local win = DiscordLib:Window("Unofficial GUI")
+
+local serv = win:Server("Joony's UnOfficial GUI", "")
+
+local btns = serv:Channel("Buttons")
+
+btns:Button(
+    "Auto Official",
+    function()
+        getgenv().Loop = true
+        DiscordLib:Notification("Notification", "Enabled Call Official!", "Okay!")
+        while wait() do -- while you wait it will do
+            if getgenv().Loop == true then -- if game environment is set to true then
+            game:GetService("ReplicatedStorage").Remotes.CallOfficial:FireServer()
+            else
+                print("off") -- continuously prints, this can help for letting you know when it is off and on
+            end
+        end
     end
-end
+)
+btns:Button(
+    "Auto Slapon6",
+    function()
+        getgenv().Slapon6 = true
+        DiscordLib:Notification("Notification", "Enabled Slap On 6!", "Okay!")
+        while wait() do -- while you wait it will do
+            if getgenv().Slapon6 == true then -- if game environment is set to true then
+            game:GetService("ReplicatedStorage").Remotes.SlapOnSix:FireServer()
+            else
+                print("off") -- continuously prints, this can help for letting you know when it is off and on
+            end
+        end
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Turn Off Auto Official",
+    function()
+        DiscordLib:Notification("Notification", "Turned off! Make sure to enable it by clicking the button above again", "Okay idiot!")
+        getgenv().Loop = false
+    end
+)
+btns:Button(
+    "Turn Off Slapon6",
+    function()
+        DiscordLib:Notification("Notification", "Turned off! Make sure to enable it by clicking the button above again", "Okay idiot!")
+        getgenv().Slapon6 = false
+    end
+)
+
+
+serv:Channel("By JoonyWoony")
+
+win:Server("Main", "http://www.roblox.com/asset/?id=6031075938")
