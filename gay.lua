66c1b1cbb7f83a5df729a22f9da5ccbb4f102909
Mashiro Shiina <80926013+JoonyWoony@users.noreply.github.com>
2022-08-14 15:41:51 +1000

    Update gay.lua

--- gay.lua
+++ gay.lua
@@ -3,18 +3,9 @@ local Table = {}
 local window = Lib:CreateWindow("Joony's Nigger Hub | FUCK YOU DANIEL JADRIEN AND JASON UR LITTLE FAGS")
 window:Section("Tools")
 window:Toggle("Toggle",{location = Table, flag = "Toggle"},function()
-    getgenv().Loop = false
+    getgenv().Loop = true
 end)
 window:Button("SLAP ON 6 & OFFICIAL",function()
-    while wait() do -- while you wait it will do
-        if getgenv().Loop == true then -- if game environment is set to true then
-        game:GetService("ReplicatedStorage").Remotes.CallOfficial:FireServer()
-     -- continuously fires the remotes 
-            print("Slap on six auto") -- continuously prints, this can help for letting you know when it is off and on
-        else
-            print("off") -- continuously prints, this can help for letting you know when it is off and on
-        end
-    end
 end)
 window:Slider("Slider",{location = Table, min = 1, max = 64, default = 32, precise = true --[[ 0.00 instead of 0 ]], flag = "Slider"},function()
    print(Table["Slider"])
@@ -23,7 +14,18 @@ window:Dropdown("Dropdown",{location = Table,flag = "Dropdown",search = true --[
    print(Table["Dropdown"])
 end)
 window:Bind("KeyBind",{location = Table, flag = "KeyBind", default = Enum.KeyCode.B},function() -- Automatically stops when the gui is removed
-    print('shut up kid')
+    getgenv().Loop = false
 end)
 window:Search(Color3.fromRGB(255,0,255) --[[nil = Yellow]]) -- Ez searcher for if you have a lot of things
 window:String({string = "String"})
+
+while wait() do -- while you wait it will do
+    if getgenv().Loop == true then -- if game environment is set to true then
+    game:GetService("ReplicatedStorage").Remotes.CallOfficial:FireServer()
+    
+ -- continuously fires the remotes 
+        print("Slap on six auto") -- continuously prints, this can help for letting you know when it is off and on
+    else
+        print("off") -- continuously prints, this can help for letting you know when it is off and on
+    end
+end
